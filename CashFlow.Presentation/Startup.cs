1a705930c90a5edd6d252287f02a63e7a439b801
Martin <mmolin@telia.com>
2020-08-17 19:43:19 +0200

    Remove using statements

--- CashFlow.Presentation/Startup.cs
+++ CashFlow.Presentation/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -35,7 +30,7 @@ namespace CashFlow.Presentation
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: default, 
+                    name: default,
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
