bf1bbee8ef1b2cd81f3e3da617bbc122ffff4a2d
Martin <mmolin@telia.com>
2020-08-21 23:29:40 +0200

    Add Dapper and repository interface

---
+++ CashFlow.Core/Interfaces/IExpenditureRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashFlow.Core.Interfaces
+{
+    interface IExpenditureRepository
+    {
+        void AddExpenditure();
+        void GetExpenditureById();
+        void UpdateExpenditure();
+        void DeleteExpenditure();
+    }
+}
