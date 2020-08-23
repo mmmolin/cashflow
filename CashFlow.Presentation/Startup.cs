7a6e9107b69aba453a0c909a053dceb1a3c27905
Martin <mmolin@telia.com>
2020-08-23 06:46:15 +0200

    Add References

--- CashFlow.Presentation/Startup.cs
+++ CashFlow.Presentation/Startup.cs
@@ -1,3 +1,5 @@
+using CashFlow.Core.Interfaces;
+using CashFlow.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,8 @@ namespace CashFlow.Presentation
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<IExpenditureRepository, ExpenditureRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
