9280f1ea446a38945aea7ce8a51117d588a2a340
Martin <mmolin@telia.com>
2020-08-29 07:14:08 +0200

    Add automapper

---
+++ CashFlow.Presentation/Profiles/ExpenditureProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CashFlow.Core.Entities;
+using CashFlow.Presentation.ViewModels;
+
+namespace CashFlow.Presentation.Profiles
+{
+    public class ExpenditureProfile : Profile
+    {
+        public ExpenditureProfile()
+        {
+            CreateMap<Expenditure, ExpenditureViewModel>();
+            CreateMap<ExpenditureViewModel, Expenditure>();
+        }
+    }
+}
