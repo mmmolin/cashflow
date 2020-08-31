38f24b792441f61c7462286ce5e306e44575ca9c
Martin <mmolin@telia.com>
2020-08-31 20:05:45 +0200

    Add navbar in _Layout

---
+++ CashFlow.Presentation/wwwroot/js/Site.js
@@ -0,0 +1,8 @@
+﻿function hamburgerMenu() {
+    let element = document.getElementById("topnav");
+    if (element.className === "topnav") {
+        element.className += " responsive";
+    } else {
+        element.className = "topnav";
+    }
+}
