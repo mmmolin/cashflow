5b35adc4b24588b05b25de32a97419da69bd27d1
Martin <mmolin@telia.com>
2020-09-02 21:59:17 +0200

    Clean up using statements

--- CashFlow.Infrastructure/Data/ExpenditureRepository.cs
+++ CashFlow.Infrastructure/Data/ExpenditureRepository.cs
@@ -1,9 +1,7 @@
 ﻿using CashFlow.Core.Entities;
 using CashFlow.Core.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace CashFlow.Infrastructure.Data
 {
