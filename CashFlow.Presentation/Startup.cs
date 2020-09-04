ee284a1a3dd2f7c0906ad08aae03c17375d126ad
Martin <mmolin@telia.com>
2020-09-04 04:58:20 +0200

    Get connectionstring from appsettings.json

--- CashFlow.Presentation/Startup.cs
+++ CashFlow.Presentation/Startup.cs
@@ -1,9 +1,11 @@
 using AutoMapper;
+using CashFlow.Core.Entities;
 using CashFlow.Core.Interfaces;
 using CashFlow.Infrastructure.Data;
 using CashFlow.Presentation.Profiles;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,10 +13,16 @@ namespace CashFlow.Presentation
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DatabaseOption>(configuration.GetSection("ConnectionStrings"));
+
             services.AddAutoMapper(typeof(ExpenditureProfile));
 
             services.AddControllersWithViews();
