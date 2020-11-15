e9f084f696620a163a70ea0a2c3bd8bdf8605bfb
Martin <mmolin@telia.com>
2020-11-16 00:32:03 +0100

    Changed pie chart background color, added Chart Service in Core

---
+++ CashFlow.Core/Services/ChartService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashFlow.Core.Services
+{
+    public class ChartService
+    {
+        public int[] CalculatePieChartData()
+        {
+            return null;
+        }
+    }
+}
